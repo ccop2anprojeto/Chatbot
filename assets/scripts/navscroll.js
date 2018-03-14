0e400e9e0a864ab351d1fe799740211834f470df
ccop2anprojeto <ccop2anprojeto@gmail.com>
2018-03-14 16:58:34 -0300

    alterações menu home

--- assets/scripts/navscroll.js
+++ assets/scripts/navscroll.js
@@ -1,7 +1,7 @@
 
 $( document ).ready( function () {     
     $( window ).scroll( function () {
-      if( $( this ).scrollTop() > 450 ) {
+      if( $( this ).scrollTop() > 300 ) {
         $( '.navbar' ).addClass( 'solid' );
       } else {
           $( '.navbar' ).removeClass( 'solid' );
