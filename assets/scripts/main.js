ae6ae33b7d24509fd03e2a5b96a9bbb21decbc2f
ccop2anprojeto <ccop2anprojeto@gmail.com>
2018-04-15 00:01:11 -0300

    alterações referentes a integraçaõ

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,43 @@
-import { teste } from 'funcionario';
+var talk = [], interaction = {};
+console.log($("#sendMsg"));
+localStorage.removeItem("talk");
 
-let bla = teste();
-console.log(bla);
+
+$("#sendMsg").on('click', function(){
+	var pergunta = $("#perg").val();
+	console.log(pergunta);
+	if( pergunta != ""){		
+		console.log("pergunta diferente de null");
+		$.get("controller.do", `command=newChatBot&perg=${pergunta}`)
+		.done(function( data ) {
+			console.log(data);
+			var Data = JSON.parse(data);
+			console.log(Data);
+			interaction["pergunta"] = pergunta;
+			interaction["resposta"] = Data[0].resp;
+		    talk.push(interaction);
+		    localStorage.setItem("talk", JSON.stringify(talk));
+		    
+		    var teste = JSON.parse(localStorage.getItem("talk"));
+			console.log(teste);
+			console.log(teste[0].pergunta);
+			
+			appendIteraction();
+
+		  });
+	}
+	$("#perg").val("");
+});
+
+var appendIteraction = () => {
+	var templateReceived, templateSend;
+	
+	
+	templateReceived = `<div class="message received"><span>${interaction.pergunta}</span></div>`;
+	
+	templateSend = `<div class="message sent"><span>${interaction.resposta}</span></div> `;
+	
+	$(".content_messages").append(templateReceived);
+	$(".content_messages").append(templateSend);
+	
+};
