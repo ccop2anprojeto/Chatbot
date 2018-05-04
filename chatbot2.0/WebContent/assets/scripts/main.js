e8eeff385a9456c87e2ec370568dc4baca627424
ccop2anprojeto <ccop2anprojeto@gmail.com>
2018-05-03 22:04:54 -0300

    arrumando bug na main

--- chatbot2.0/WebContent/assets/scripts/main.js
+++ chatbot2.0/WebContent/assets/scripts/main.js
@@ -1,43 +1,161 @@
 var talk = [], interaction = {};
+var notIdentified, controll = 0, cont = 1;
+
 console.log($("#sendMsg"));
 localStorage.removeItem("talk");
 
+$(".icon_chat a").on('click', function(){
+	
+  $(".container_chat").addClass("active");
+  $(".content_messages").addClass("expand");
+  $(".content_area").addClass("expand");
+  $(".container_chat").addClass("expand");
+  $(".icon_chat").removeClass("active");
+  var init = `começar`;
+  appendPerg(init);
+  $.get("controller.do", `command=historyIdentify&init=${init}`)
+	.done(function( data ) {
+		console.log(data[0]);
+		var Data = JSON.parse(data.toString('utf8'));
+		console.log(Data);
+		
+		appendResp(Data[0].resp);
+	  });  
+});
+var historyIdentify = (cpf) => {
+  	
+  $.get("controller.do", `command=historyIdentify&init=${cpf}`)
+	.done(function( data ) {
+		var Data = JSON.parse(data.toString('utf8'));
+		console.log(Data);
+		notIdentified = Data[Data.length-1];
+		if(notIdentified){			
+		    localStorage.setItem("data", JSON.stringify(Data));
+		}
+		appendResp(Data[0].resp);				
+		
+	  });	
+}
+
+var historyQuestions = (pergunta) => {
+	cont = 1;
+	$.get("controller.do", `command=newChatBot&perg=${pergunta}`)
+	.done(function( data ) {
+		console.log(data);
+		var Data = JSON.parse(data);
+		console.log(Data);
+		interaction["pergunta"] = pergunta;
+		interaction["resposta"] = Data[0].resp;
+	    talk.push(interaction);
+	    localStorage.setItem("talk", JSON.stringify(talk));
+	    localStorage.setItem("respostas", JSON.stringify(Data));
+	    var teste = JSON.parse(localStorage.getItem("talk"));
+		console.log(teste);
+		
+		appendResp(interaction.resposta);
+		feedBackResp();
+				
+		
+        
+	  });
+	
+}
+
+var feedBackResp = () => {
+	var templateSend = `<div class="message sent">Satisfeito(a) com a resposta?<span></span><div class="content_buttons"><button type="button" id="sim" class="btn btn-success sim">Sim</button><button type="button" id="nao" class="btn btn-warning nao">Não</button></div></div> `;
+	
+	$(".content_messages").append(templateSend);
+	
+	$(".sim").on('click', function(){
+		templateSend = `<div class="message sent"><span>Fico feliz por ter tirado sua dúvida :)</span></div> <div class="message sent"><span>Posso te ajudar em mais alguma coisa?</span></div>`;
+		$(".content_messages").append(templateSend);
+	});
+	
+	$(".nao").on('click', function(){			
+		console.log("clicou");
+		let data = JSON.parse(localStorage.getItem("respostas"));
+		
+		if(cont > 2){
+			templateSend = `<div class="message sent">O atendimento será transferido para um atendente humano.<br/> Aguarde alguns instantes você será atendido.<span></span></div> `;
+			$(".content_messages").append(templateSend);			
+		}
+			
+		else{
+			appendResp(data[cont].resp);
+			feedBackResp();
+		}
+			
+		//$(".content_messages").append(templateSend);
+		cont++;
+	});		
+	
+}
+
+
+var counter = 1;
+var register = [];
+var Data = JSON.parse(localStorage.getItem("data"));
+var cpf;
+console.log(Data.length);
 
 $("#sendMsg").on('click', function(){
 	var pergunta = $("#perg").val();
+	
 	console.log(pergunta);
 	if( pergunta != ""){		
-		console.log("pergunta diferente de null");
-		$.get("controller.do", `command=newChatBot&perg=${pergunta}`)
-		.done(function( data ) {
-			console.log(data);
-			var Data = JSON.parse(data);
-			console.log(Data);
-			interaction["pergunta"] = pergunta;
-			interaction["resposta"] = Data[0].resp;
-		    talk.push(interaction);
-		    localStorage.setItem("talk", JSON.stringify(talk));
-		    
-		    var teste = JSON.parse(localStorage.getItem("talk"));
-			console.log(teste);
-			console.log(teste[0].pergunta);
+		appendPerg(pergunta);
+		if(controll == 0){
+			register.push(pergunta);
+			historyIdentify(pergunta);
 			
-			appendIteraction();
-
-		  });
+		}else{
+			if(!notIdentified){
+				historyQuestions(pergunta);								
+			}			
+				
+			
+			else{			
+				if(counter < Data.length){
+					register.push(pergunta);
+					if(counter < Data.length -1)
+						appendResp(Data[counter].resp);
+					
+					counter++;
+				}						
+				if(counter >= Data.length){
+					$.get("controller.do", `command=historyRegister&Cpf=${register[0]}&Nome=${register[2]}&Telefone=${register[3]}&Email=${register[4]}`)
+					.done(function( data ) {
+						var Data = JSON.parse(data.toString('utf8'));
+						console.log(Data);
+								
+						//localStorage.setItem("data", JSON.stringify(Data));
+						if(Data.length > 1){
+							appendResp(Data[0].resp);
+							appendResp(Data[1].resp);
+						}else
+							appendResp(Data[0].resp);
+						
+						notIdentified = Data[Data.length-1];
+																	
+					  });
+				}
+			}
+		}	
+				
+										
 	}
+	controll++;
 	$("#perg").val("");
 });
+console.log(register);
 
-var appendIteraction = () => {
-	var templateReceived, templateSend;
-	
-	
-	templateReceived = `<div class="message received"><span>${interaction.pergunta}</span></div>`;
-	
-	templateSend = `<div class="message sent"><span>${interaction.resposta}</span></div> `;
-	
+var appendPerg = (perg) => {
+	var templateReceived = `<div class="message received"><span>${perg}</span></div>`;
 	$(".content_messages").append(templateReceived);
+
+}
+
+var appendResp = (resp) => {		
+	var templateSend = `<div class="message sent"><span>${resp}</span></div> `;
 	$(".content_messages").append(templateSend);
-	
 };
