f5b8823b26599aa8f21c606715d3ce2e1840dbc6
ccop2anprojeto <ccop2anprojeto@gmail.com>
2018-06-12 00:15:02 -0300

    javascript do modal -- testes

--- chatbot2.0/WebContent/assets/scripts/modal.js
+++ chatbot2.0/WebContent/assets/scripts/modal.js
@@ -10,3 +10,13 @@ $(".tooltip").on('mouseout', function(){
 $(".tooltip").on('click', function(){
     $(".modal").addClass("active");
 });
+
+$(".modal .yes").on('click', function(){
+    $(".modal").removeClass("active");
+    location.reload();
+});
+
+$(".modal .nop").on('click', function(){
+    $(".modal").removeClass("active");
+});
+
